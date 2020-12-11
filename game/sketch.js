6e93a862ee7d0531007f6fc64edf19cb34551db4
zoecyang <zoecyang@gmail.com>
2020-12-10 21:41:12 -0800

    Update sketch.js

--- game/sketch.js
+++ game/sketch.js
@@ -1,5 +1,5 @@
 //how do i get game to show in local host, its only showing the code
-let stars = [];
+let star = [];
 
 let me = [];
 
@@ -7,35 +7,52 @@ let planet = [];
 
 let moon = [];
 
+let rocket = [];
+
+
 function setup() {
   createCanvas(500, 400);
-  me = new rocket(width/2, 300, 3);
+  me = new Rocket(width/2, 300, 3);
 }
 
 function draw(){
     background(8, 39, 89);
-    stars(random(600),random(400));
 
-    me.drawMe();
     me.moveMe();
 
+  if (frameCount % 25 == 0) {
+      let  s = new Star(random(0,500), 0, -3);
+      stars.push(s);
+      console.log(stars);
+    }
 
-//if (frameCount % 25 == 0) { how do you get stars to come from top and not left side?
-    //let  s = new Stars(width, random(0,height), -3);
-  //  stars.push(s);
-  //  console.log(stars); //print the balls array to the console
-//  } acts for when the stars enter screen
+  for (let i = 0; i < stars.length; i++) {
+          stars[i].drawStar();
+          stars[i].moveStar();
+      //    stars[i].bounceStar(); set to something that makes stars disapear
+    }
 
-for (let i = 0; i < stars.length; i++) {//would it be more convenient and organized to create a function for this? esp if we will use this for planets
-        stars[i].drawStar();
-        stars[i].moveStar();
-    //    stars[i].bounceStar(); set to something that makes stars disapear
+    drawThings(Planet);
+    drawThings(Star);
+    drawThings(Moon);
+    drawThings(Me);
+}
+
+function drawThings(list) {
+  for(let i = 0; i < list.length; i++ ){
+    list[i].draw();
   }
 }
 
-class planet(){
+class Planet{
 
-  drawPlanet(){
+  constructor(x,y, speed){
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+  }
+
+  draw(){
     strokeWeight(0.0)
     ellipse(200,200,250,50)
     fill(8, 39, 89)
@@ -49,9 +66,14 @@ class planet(){
 //  }
 }
 
-class moon(){
+class Moon{
+  constructor(x,y, speed){
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+  }
 
-  drawMoon(){
+  draw(){
     strokeWeight(0.0)
     fill(232, 230, 230)
     ellipse(200,200,200)
@@ -69,46 +91,46 @@ class moon(){
 //  }
 }
 
-class rocket {
+class Rocket{
   constructor(x,y, speed){ //every avatar needs an x value, a y value, and a speed
         this.x = x;
         this.y = y;
         this.speed = speed;
   }
-}
 
-drawMe(){
+draw(){
+  scale(.5)
   strokeWeight(1);
-stroke(51);
-fill(6, 24, 145);
-circle(250,160,80);//window for avatar
-fill("white");
-circle(247,160,20); //body
-fill(255, 201, 255);
-arc(200, 170, 100, 260, 1.589, HALF_PI);//rocket
-//arc(50, 50, 80, 80, 0, PI + QUARTER_PI, CHORD);
-//arc(200, 100, 80, 80, -100, PI + QUARTER_PI, PIE);
-fill(255, 226, 201);
-circle(270,160,25);//head
+  stroke(51);
+  fill(6, 24, 145);
+  ellipse(250,160,80);//window for avatar
+  fill("white");
+  ellipse(247,160,20); //body
+  fill(255, 201, 255);
+  arc(200, 170, 100, 260, 1.589, HALF_PI);//rocket
+  //arc(50, 50, 80, 80, 0, PI + QUARTER_PI, CHORD);
+  //arc(200, 100, 80, 80, -100, PI + QUARTER_PI, PIE);
+  fill(255, 226, 201);
+  ellipse(270,160,25);//head
 
-fill("white");
-circle(200,130,40);// window on rocket top
-circle(200,185,40); //window on rocket middle (last window is 6 lines lower)
+  fill("white");
+  ellipse(200,130,40);// window on rocket top
+  ellipse(200,185,40); //window on rocket middle (last window is 6 lines lower)
 
-noStroke();
-fill(255, 201, 255);
-triangle(130, 345, 200, 205, 270, 345);//rocket jets
-strokeWeight(1);
-stroke(51);
-fill("white");
-circle(200,240,40);//window on rocket last
+  noStroke();
+  fill(255, 201, 255);
+  triangle(130, 345, 200, 205, 270, 345);//rocket jets
+  strokeWeight(1);
+  stroke(51);
+  fill("white");
+  ellipse(200,240,40);//window on rocket last
 
-fill("orange");
-triangle(130, 345, 155, 390, 174, 345);
-triangle(175, 345, 200, 390, 222, 345);
-triangle(222, 345, 245, 390, 269, 345);
-  //code for rocket ship
-}
+  fill("orange");
+  triangle(130, 345, 155, 390, 174, 345);
+  triangle(175, 345, 200, 390, 222, 345);
+  triangle(222, 345, 245, 390, 269, 345);
+    //code for rocket ship
+  }
 
 moveMe(){
   if (keyIsDown(UP_ARROW)) { //if you hold the up arrow, move up by speed
@@ -124,28 +146,32 @@ moveMe(){
     this.x -= this.speed;
   }
 }
+}
 
-class stars {
-//
+
+class Star {
+
   constructor(x,y, speed){
 		this.x = x;
     this.y = y;
     this.speed = speed;
 	}
 
-  drawStars(){
-    textSize(50)//should this be a function?
-    text("⭐",x,y);
+  draw(){//trade in code for stars later
+    stroke(0);
+    strokeWeight(1);
+    fill(50,150,60);
+    ellipse(this.x,this.y,10,10);
   }
 
-  moveStars(){
+  moveStar(){
     this.x = this.x+ this.speed;
-    this.y = this.y+ this.speed;
+    this.y = this.y+ this.speed;//does this have to do with the direction the stars go?
   }
+}
 
   //bounceStars(){ set to something that makes stars disapear
   //  if (this.x >= me.x-10 && this.x <= me.x+10 && this.y > me.y-40 && this.y < me.y+80){
         //  this.speed = -this.speed;
   //   }
 //  }
-}
